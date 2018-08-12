9c9d3d05341f10d168860e800caa2fa44a5fe3fd
Brian Yang <brian.yang328@gmail.com>
2018-08-11 21:27:43 -0400

    Fix home content

--- volunteer/src/Home/home-content.js
+++ volunteer/src/Home/home-content.js
@@ -30,6 +30,7 @@ export class HeaderContent extends React.Component {
           </div>
           <div className="small sigmarone">
             <p>Volunteer and win prizes and glory. It's that simple. </p>
+          </div>
           <div className="small">
             <p>Charity Park is a kickstarter for businesses, communities and people with shared goals around the world.</p>
           </div>
